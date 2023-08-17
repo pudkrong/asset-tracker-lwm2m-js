ac2c87e0a51ca5789accdbfe007fcdaa4c4af73b
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-08-17 15:52:45 +0200

    docs: add config schema

---
+++ schemas/Config_50009.ts
@@ -0,0 +1,27 @@
+/**
+ * Asset Tracker Configuration
+ *
+ * Asset Tracker v2 configuration object
+ *
+ * @see https://github.com/NordicSemiconductor/asset-tracker-cloud-firmware-aws/blob/saga/src/cloud/lwm2m_integration/config_object_descript.xml
+ *
+ * ID: 50009
+ * LWM2MVersion:
+ * ObjectVersion:
+ * MultipleInstances: false
+ * Mandatory: false
+ */
+export type Config_50009 = Readonly<{
+	'0': boolean
+	'1': number
+	'2': number
+	'3': number
+	'4': number
+	'5': number
+	'6': boolean
+	'7': boolean
+	'8': number
+	'9': number
+}>
+
+export const Config_50009_urn = '50009'
